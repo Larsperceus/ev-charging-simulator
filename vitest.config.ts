153a620c22e7c80eaee0cb2882f9e1839922bdfd
Lars Kammeijer <lars.kammeijer@resillion.com>
2026-03-18 11:27:27 +0100

    chore: remove unneeded pool config from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    pool: 'forks',
     testTimeout: 15000,
     exclude: ['dist/**', 'node_modules/**'],
     coverage: {
